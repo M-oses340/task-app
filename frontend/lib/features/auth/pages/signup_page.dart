d3b1c2d976b9eb673c51901d8ada833b6322a386
Moses Omwa <mosesomwa7@gmail.com>
2025-11-17 08:28:06 +0300

    signup page

--- frontend/lib/features/auth/pages/signup_page.dart
+++ frontend/lib/features/auth/pages/signup_page.dart
@@ -34,7 +34,50 @@ class _SignupPageState extends State<SignupPage> {
                 fontWeight: FontWeight.bold,
               ),
             ),
-            TextFormField(),
+            const SizedBox(height: 30),
+            TextFormField(
+              controller: nameController,
+              decoration: const InputDecoration(
+                hintText: 'Name',
+              ),
+              validator: (value) {
+                if (value == null || value.trim().isEmpty) {
+                  return "Name field cannot be empty!";
+                }
+                return null;
+              },
+            ),
+            const SizedBox(height: 15),
+            TextFormField(
+              controller: emailController,
+              decoration: const InputDecoration(
+                hintText: 'Email',
+              ),
+              validator: (value) {
+                if (value == null ||
+                    value.trim().isEmpty ||
+                    !value.trim().contains("@")) {
+                  return "Email field is invalid!";
+                }
+                return null;
+              },
+            ),
+            const SizedBox(height: 15),
+            TextFormField(
+              controller: passwordController,
+              decoration: const InputDecoration(
+                hintText: 'Password',
+              ),
+              validator: (value) {
+                if (value == null ||
+                    value.trim().isEmpty ||
+                    value.trim().length <= 6) {
+                  return "Password field is invalid!";
+                }
+                return null;
+              },
+            ),
+
           ],
         ),
       ),
