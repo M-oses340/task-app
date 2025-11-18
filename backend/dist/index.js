665703e18d5f05925fc49d8f5ff20bce035043bb
Moses Omwa <mosesomwa7@gmail.com>
2025-11-18 07:29:18 +0300

    docker

--- backend/dist/index.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-const app = express();
-app.get("/", (req, res) => {
-    res.send("Welcome to my app!");
-});
-app.listen(8000, () => {
-    console.log("Server started");
-});
-//# sourceMappingURL=index.js.map
