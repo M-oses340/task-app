43b6ca1f78a7a52fe75b69923144a0b8703085e1
Moses Omwa <mosesomwa7@gmail.com>
2025-11-18 06:55:11 +0300

    json configuration

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,6 +2,10 @@ import express from "express";
 
 const app = express();
 
+app.get("/", (req, res) => {
+  res.send("Welcome to my app!!!");
+});
+
 app.listen(8000, () => {
     console.log("Server started");
 
