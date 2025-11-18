5fe838d2eabe04bc551fc487c63cb66c2cca3529
Moses Omwa <mosesomwa7@gmail.com>
2025-11-18 09:10:16 +0300

    docker

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -3,7 +3,7 @@ import express from "express";
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("Welcome to my app");
+  res.send("Welcome to my app!!!!");
 });
 
 app.listen(8000, () => {
